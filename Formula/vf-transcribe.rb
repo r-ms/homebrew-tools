a5c34886fc5371fd00d6239845f81995abd9e90a
Mikhail Rakutko <m.rakutko@gmail.com>
2026-02-10 16:13:19 +0000

    fix: point formula URLs to homebrew-tools releases (single repo)

--- Formula/vf-transcribe.rb
+++ Formula/vf-transcribe.rb
@@ -4,10 +4,10 @@ class VfTranscribe < Formula
   version "0.1.0"
 
   if Hardware::CPU.arm?
-    url "https://github.com/r-ms/vf-transcribe-releases/releases/download/v0.1.0/vf-transcribe-0.1.0-darwin-arm64.tar.gz"
+    url "https://github.com/r-ms/homebrew-tools/releases/download/v0.1.0/vf-transcribe-0.1.0-darwin-arm64.tar.gz"
     sha256 "df2af80072139691471989fbc6513933cb5c85ab70117c663084703635771f1d"
   else
-    url "https://github.com/r-ms/vf-transcribe-releases/releases/download/v0.1.0/vf-transcribe-0.1.0-darwin-x86_64.tar.gz"
+    url "https://github.com/r-ms/homebrew-tools/releases/download/v0.1.0/vf-transcribe-0.1.0-darwin-x86_64.tar.gz"
     sha256 "PLACEHOLDER_X86_64_SHA256"
   end
 
